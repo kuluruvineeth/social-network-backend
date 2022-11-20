a1a0ef41d0f407c03ea2294460db0202fed65c30
Kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-11-20 17:28:50 +0530

    Initial commit

---
+++ src/main/kotlin/com/kuluruvineeth/Application.kt
@@ -0,0 +1,17 @@
+package com.kuluruvineeth
+
+import io.ktor.server.application.*
+import com.kuluruvineeth.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSockets()
+    configureSerialization()
+    configureMonitoring()
+    configureHTTP()
+    configureSecurity()
+    configureRouting()
+}
