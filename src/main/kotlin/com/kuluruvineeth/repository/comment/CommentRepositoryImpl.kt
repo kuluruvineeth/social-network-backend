28066c71b348f2eaceea14c6663c6da0ec2de47d
Kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-11-27 11:51:11 +0530

    Increased comment count when comment was created

--- src/main/kotlin/com/kuluruvineeth/repository/comment/CommentRepositoryImpl.kt
+++ src/main/kotlin/com/kuluruvineeth/repository/comment/CommentRepositoryImpl.kt
@@ -2,19 +2,24 @@ package com.kuluruvineeth.repository.comment
 
 import com.kuluruvineeth.data.models.Comment
 import com.kuluruvineeth.data.models.Like
+import com.kuluruvineeth.data.models.Post
 import com.kuluruvineeth.data.responses.CommentResponse
 import org.litote.kmongo.and
 import org.litote.kmongo.coroutine.CoroutineDatabase
 import org.litote.kmongo.eq
+import org.litote.kmongo.setValue
 
 class CommentRepositoryImpl(
     db: CoroutineDatabase
 ) : CommentRepository{
 
+    private val posts = db.getCollection<Post>()
     private val comments = db.getCollection<Comment>()
     private val likes = db.getCollection<Like>()
     override suspend fun createComment(comment: Comment) : String{
         comments.insertOne(comment)
+        val oldCommentCount = posts.findOneById(comment.postId)?.commentCount ?: 0
+        posts.updateOneById(comment.postId, setValue(Post::commentCount,oldCommentCount+1))
         return comment.id
     }
 
